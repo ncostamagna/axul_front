23760e530f86239b3d45849d892c6bcbf7e07469
Nahuel Costamagna <nlcostamagna@gmail.com>
2021-10-25 21:47:08 -0300

    create redux store

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import Login from './components/login/Login'
 import Router from './components/router/Router'
 
+import { Provider } from 'react-redux';
+import store from './store';
 
 const App = () => {
     let login = false;
 
     return (
-        <div className='container mx-auto'>
-            {
-                login == true?<Router />:<Login />
-            }
-        </div>
+        <Provider store={store}>
+            <div className='container mx-auto'>
+                {
+                    login == true?<Router />:<Login />
+                }
+            </div>
+        </Provider>
     )
 }
 
