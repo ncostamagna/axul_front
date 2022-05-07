c5f8c83cbe2b3876405dabc98baf4662f4b2c913
Nahuel <nlcostamagna@gmail.com>
2022-05-07 01:29:38 -0300

    upgrate style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,36 @@ import Menu from '../../components/menu';
 import {Contacts} from '../../util/api'
 import {useState} from 'react'
 
+const birhtdayStyle = (days) => {
+  
+  let popup = {
+    backgroundColor: "rgba(255,255,255,.95)",
+    };
+
+  console.log(days)
+  switch (days) {
+    case 0:
+      console.log("entra")
+      popup['backgroundColor'] = "rgba(64,167,0,.15)";
+      break;
+  
+    default:
+      break;
+  }
+
+  return popup
+  
+}
+
+const socialWA = (days, nickname, phone) => {
+
+  if (days == 0) {
+    return  <a href={`whatsapp://send?text=${nickname}%20muuuy%20feliz%20cumple!!%0AEspero%20que%20lo%20disfrutes%20al%20maximo%20en%20tu%20dia!!%0ATe%20deseo%20lo%20mejor%20para%20este%20nuevo%20año!!%0AMuchos%20exitooos!!%20:)&phone=${phone}`} className="social-link mt-1 align-middle"><i className="fa fa-whatsapp"></i></a>
+  }
+
+  return <li className="list-inline-item"></li>
+}
+
 function Home() {
   const [contacts, setContacts] = useState([]);
   useEffect( () => {
@@ -23,35 +53,60 @@ function Home() {
       localStorage.removeItem("axul_user_token");
       localStorage.removeItem("axul_user_id");
 
-  })
-  }, [])
+  })}, [])
 
   console.log(contacts)
   return (
     <div>
     <Menu></Menu>
     <Container>
-    <Row className='text-center'>
-      { 
-      contacts.map(c => <div key={c.id} class="col-xl-3 col-sm-6 mb-5">
-            <div class="bg-white rounded shadow-sm py-5 px-4">
-              <img src="https://bootstrapious.com/i/snippets/sn-team/teacher-4.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm mx-auto" />
-                <h5 class="mb-0">{c.firstname + " " + c.lastname}</h5><span class="small text-uppercase text-muted">{c.birthday}</span>
-                <ul class="social mb-0 list-inline mt-3">
-                    <li class="list-inline-item">
-                      <a href={`whatsapp://send?text=${c.nickname}%20muuuy%20feliz%20cumple!!%0AEspero%20que%20lo%20disfrutes%20al%20maximo%20en%20tu%20dia!!%0ATe%20deseo%20lo%20mejor%20para%20este%20nuevo%20año!!%0AMuchos%20exitooos!!%20:)&phone=${c.phone}`} class="social-link"><i class="fa fa-whatsapp"></i></a>
-                    </li>
-                </ul>
+      <Row className='mt-3 d-flex '>
+        { 
+        contacts.map(c => 
+          <div key={c.id} className="col-12 mb-5">
+            <div className="rounded shadow-sm py-4 px-4" style={birhtdayStyle(c.days)}>
+              <Row>
+                <div className="col-9">
+                <h5 className="mb-0">{c.firstname + " " + c.lastname}</h5><span className="small text-muted h5">{birthdayFormat(c.birthday, c.days)}</span>
+              </div>
+              <div className="col-3">
+                {
+                  socialWA(c.days, c.nickname, c.phone)
+                }
+              </div>
+              
+              </Row>
+              
             </div>
           </div>
-        ) 
-      }
-
-      
+          ) 
+        }
       </Row>
     </Container>
     </div>
   );
 }
+
+
+const birthdayFormat = (value, days) => {
+
+    let date = value.split("T")[0];
+    date = date.split("-");
+
+    let daysLabel = ""
+    switch (days) {
+      case 1:
+        daysLabel = "(1 Dia)"
+        break;
+      case 0:
+        break;
+      default:
+        daysLabel = `(${days} Dias)`
+        break;
+    }
+    return `${date[2]}/${date[1]} ${daysLabel}`
+}
+
+
 
 export default Home;
